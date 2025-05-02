38bd6b485712cd57f783f13cf9f749cb781f0142
rohit-godara <ROHITGODARASELF@Rohits-MacBook-Pro.local>
2025-05-02 22:44:32 +0530

    Your interest starts now! Loan Mate

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='py-4'>
+        <h1 className='text-center text-4xl font-bold'>Loan Mate</h1>
+    </div>
+  )
+}
+
+export default Navbar
