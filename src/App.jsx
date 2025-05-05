11419a0718a2a3f14788b2b72722afb3984452bf
rohit-godara <ROHITGODARASELF@Rohits-MacBook-Pro.local>
2025-05-05 21:49:15 +0530

    Loan Calculator Ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import HeroSection from './HeroSection'
+import CalculatorPage from './CalculatorPage'
 
 const App = () => {
   return (
-    <div className='h-screen w-full bg-black/95 text-white '> 
-      <Navbar />
-    </div>
+      <Router>
+        <Routes>
+          <Route path='/' element={<HeroSection />} />
+          <Route path='/Calculator' element={<CalculatorPage />} />
+        </Routes>
+      </Router>
   )
 }
 
